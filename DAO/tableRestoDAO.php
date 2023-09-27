51673dd2fb457d5843d0630b4e228b70b1a738e5
hugolagouardat <45795395+hugolagouardat@users.noreply.github.com>
2023-09-27 09:43:01 +0200

    tab restodao

--- DAO/tableRestoDAO.php
+++ DAO/tableRestoDAO.php
@@ -1,13 +1,33 @@
 <?php
 
-	class tableRestoDAO {
-		public static function getAlltable()
-		{
-		
-		}
-		public static function get($id)
-		{
-			
-		}
+include_once("tools/RequestSender.php");
 
-	}
+class tableRestoDAO {
+
+    public static function getAlltable() {
+        $tables = array();
+        $resultats = RequestSender::sendGetRequest("table");
+        $resultats = json_decode($resultats);
+        foreach ($resultats as $result) {
+            $table = new tableRestoDTO($result->idTable, $result->etat);
+            $table->setIdTable($result->idTable);
+            $tables[] = $table;
+        }
+
+        return $tables;
+    }
+
+    public static function get($id) {
+        $table = null;
+        $resultats = RequestSender::sendGetRequest("table");
+        $resultats = json_decode($resultats);
+        if (sizeof($resultats) > 0) {
+            $result = $resultats[0];
+            $table = new tableRestoDTO($result->idTable, $result->etat);
+            $table->setIdTable($result->idTable);
+        }
+
+        return $table;
+    }
+
+}
