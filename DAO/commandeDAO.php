b157c0ba0acdc01118dccaf152f96589cbf2fe9c
elpaulo63 <tourret.paul@gmail.com>
2023-09-27 09:36:20 +0100

    get + getall

--- DAO/commandeDAO.php
+++ DAO/commandeDAO.php
@@ -5,10 +5,27 @@ class commandeDAO
 
 	public static function getAllcommandes()
 	{
-		
+		$commande = array();
+        $resultats = RequestSender::sendGetRequest("commandes");
+        $resultats = json_decode($resultats);
+        foreach ($resultats as $result) {
+            $commandee = new CommandeDTO($result->commandeId, $result->tableId, $result->effectue);
+            $commande[] = $commandee;
+        }
+
+        return $commande;		
 	}
 	public static function get($id)
     {
-       
+		$commande = null;
+		$resultats = RequestSender::sendGetRequest("commandes");
+		$resultats = json_decode($resultats);
+        if (sizeof($resultats) > 0) {
+            $result = $resultats[0];
+			$commande= new CommandeDTO($result->idCommande, $result->tableId,$result->effectue);
+			$commande->setIdCommande($result->idCommande);
+        }
+
+        return $commande;
     }
 }
