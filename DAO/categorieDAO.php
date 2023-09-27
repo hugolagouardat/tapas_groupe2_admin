af19631ed49df1e9b6a963043d3a3e4120e782a6
elpaulo63 <tourret.paul@gmail.com>
2023-09-27 09:38:42 +0100

    re test

--- DAO/categorieDAO.php
+++ DAO/categorieDAO.php
@@ -4,10 +4,29 @@ class categorieDAO
 {
     public static function getAllcategories()
     {
+        $categorie = array();
+        $resultats = RequestSender::sendGetRequest("categories");
+        $resultats = json_decode($resultats);
+        foreach ($resultats as $result) {
+            $categoriee = new categorieDTO($result->idCategorie, $result->libelle);
+            $categoriee->setIdcategorie($result->idCategorie);
+            $categorie[] = $categoriee;
+        }
+
+        return $categorie;
         
     }
     public static function get($id)
     {
-        
+        $categoriee = null;
+        $resultats = RequestSender::sendGetRequest("categories");
+        $resultats = json_decode($resultats);
+        if (sizeof($resultats) > 0) {
+            $result = $resultats[0];
+            $categoriee = new categorieDTO($result->idCategorie, $result->libelle);
+            $categoriee->setIdcategorie($result->idCategorie);
+        }
+
+        return $categoriee;
     }
 }
