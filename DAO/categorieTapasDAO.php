50faa55a8282bda9574596ea49744156c6268b93
hugolagouardat <45795395+hugolagouardat@users.noreply.github.com>
2023-10-18 11:53:32 +0200

    modif menu et correction catégorie tapas DAO

--- DAO/categorieTapasDAO.php
+++ DAO/categorieTapasDAO.php
@@ -4,53 +4,55 @@ include_once("DTO/CategorieTapasDTO.php");
 
 class CategorieTapasDAO
 {
+
     public static function getAllCategorieTapas()
     {
-        $CategoriesTapas = array();
-        $resultat = RequestSender::sendGetRequest("categoriesTapas");
-        $resultats = json_decode($resultat);
-        foreach ($resultats as $result) {
-            $CategorieTapas = new categorieTapasDTO($result->CategorieId, $result->tapasId);
-            $CategorieTapas->setCategorieId($result->CategorieId);
-            $CategorieTapas->setTapasId($result->tapasId);
-            $CategoriesTapas = $CategorieTapas;
+        $categorie = array();
+        $resultats = RequestSender::sendGetRequest("categoriesTapas");
+        $resultats = json_decode($resultats, true);
+        if (is_array($resultats)) {
+            foreach ($resultats as $result) {
+                $categoriee = new CategorieTapasDTO($result["CategorieId"], $result["tapasId"]);
+                $categorie[] = $categoriee;
+            }
         }
-        return $CategoriesTapas;
+        return $categorie;
+
     }
     // fonctionne pas //
-	public static function get($id)
-	{
-		$resultats = RequestSender::sendGetRequest("categoriesTapas/" . $id);
-		$resultats = json_decode($resultats, true);
-		return $resultats;
-	}
-    
-    public static function update($tapasId,$categorieId){
+    public static function get($id)
+    {
+        $resultats = RequestSender::sendGetRequest("categoriesTapas/" . $id);
+        $resultats = json_decode($resultats, true);
+        return $resultats;
+    }
+
+    public static function update($tapasId, $categorieId)
+    {
         $data = array(
             "tapasId" => $tapasId,
             "categorieId" => $categorieId
         );
-        
+
         var_dump($data);
-        $inserted = RequestSender::sendPutRequest("categories",$data);
+        $inserted = RequestSender::sendPutRequest("categories", $data);
         return $inserted;
-
     }
 
-    public static function delete($id){
-        $delete = RequestSender::sendDeleteRequest("categoriesTapas/".$id);
-		return $delete; 
-
+    public static function delete($id)
+    {
+        $delete = RequestSender::sendDeleteRequest("categoriesTapas/" . $id);
+        return $delete;
     }
-    public static function insert($tapasId,$categorieId){
+    public static function insert($tapasId, $categorieId)
+    {
         $data = array(
             "tapasId" => $tapasId,
             "categorieId" => $categorieId
         );
-        
+
         var_dump($data);
-        $inserted = RequestSender::sendPostRequest("categories",$data);
+        $inserted = RequestSender::sendPostRequest("categories", $data);
         return $inserted;
     }
 }
-?>
--- pages/menu/menu.php
+++ pages/menu/menu.php
@@ -1,5 +1,4 @@
 <?php
-
 $categoriesRelations = CategorieTapasDAO::getAllCategorieTapas();
 ?>
 
@@ -12,42 +11,54 @@ $categoriesRelations = CategorieTapasDAO::getAllCategorieTapas();
     <title>Modif Menu</title>
 
     <!-- Bootstrap CSS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
 <div class="container mt-4">
+    <div class="card mb-4">
+        <div class="card-header d-flex justify-content-between">
+            <h5 class="mb-0">Création / Modification</h5>
+            <div>
+                <button class="btn btn-primary">New Tapas</button>
+                <button class="btn btn-warning">Modif Catégorie</button>
+                <button class="btn btn-primary">Retour au commande</button>
 
-    <?php 
-    //$handledCategories = []; 
-
-    foreach ($categoriesRelations as $relation) : 
-
-        //if (!in_array($relation->getCategorieId(), $handledCategories)) :
-            //array_push($handledCategories, $relation->getCategorieId());
-    ?>
-
-        <div class="card mb-4">
-            <div class="card-header">
-                Categorie: <?php echo $relation->getCategorieId(); ?>
-            </div>
-            <div class="card-body">
-                <div class="row">
-
-
-                </div>
             </div>
         </div>
+    </div>
 
-    <?php 
-        //endif;
-    endforeach; 
-    ?>
+    <div class="row">
+        <?php
+        $tapasList = tapasDAO::getAlltapas();
+        foreach ($tapasList as $tapas) :
+            // Ici, vous devez ajouter une logique pour récupérer les catégories pour le tapas actuel.
+            $categoriesForCurrentTapas = []; // Utilisez une méthode pour cela
+        ?>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="http://127.0.0.1/projet/restaurant_tapas/tapas_groupe2_webservice/src/images/<?php echo $tapas->getImage(); ?>" class="card-img-top" alt="Image de Tapas">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $tapas->getNom(); ?></h5>
+                        <p class="card-text"><?php echo $tapas->getDescription(); ?></p>
+                        <p class="card-text"><strong>Prix:</strong> <?php echo $tapas->getPrix(); ?>€</p>
 
+                        <?php foreach ($categoriesForCurrentTapas as $category): ?>
+                            <p class="card-text"><strong>Catégorie:</strong> <?php echo $category; ?></p>
+                        <?php endforeach; ?>
+
+                        <a href="#" class="btn btn-primary">Modification</a>
+                        <a href="#" class="btn btn-danger">Suppression</a>
+                    </div>
+                </div>
+            </div>
+        <?php
+        endforeach;
+        ?>
+    </div>
 </div>
-
 </body>
+
 </html>
